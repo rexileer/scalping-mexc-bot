a9acbb6d6fe883740ffc38990875bb073cb8d9ac
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-10 21:43:58 +0300

    Update set_commands.py

--- bot/utils/set_commands.py
+++ bot/utils/set_commands.py
@@ -1,12 +1,19 @@
-from aiogram import Bot
 from aiogram.types import BotCommand
+from aiogram import Bot
 
 async def set_default_commands(bot: Bot):
     commands = [
-        BotCommand(command="/start", description="Запустить бота"),
-        BotCommand(command="/menu", description="Главное меню"),
-        BotCommand(command="/auth", description="Авторизоваться в MEXC"),
-        BotCommand(command="/balance", description="Показать баланс"),
-        BotCommand(command="/settings", description="Настройки"),
+        BotCommand(command="/balance", description="Баланс"),
+        BotCommand(command="/price", description="Прайс"),
+        BotCommand(command="/autobuy", description="Автопокупка"),
+        BotCommand(command="/stop", description="Остановить автопокупку"),
+        BotCommand(command="/buy", description="Покупка"),
+        BotCommand(command="/status", description="Статус"),
+        BotCommand(command="/params", description="Параметры"),
+        BotCommand(command="/faq", description="FAQ"),
+        BotCommand(command="/pair", description="Торговая пара"),
+        BotCommand(command="/subscription", description="Подписка"),
+        BotCommand(command="/stats", description="Статистика"),
+        BotCommand(command="/auth", description="Авторизация"),
     ]
     await bot.set_my_commands(commands)
