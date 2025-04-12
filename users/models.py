bdf634b529da664105a02803f65486683e11c92f
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-12 23:45:55 +0300

    update user model

--- users/models.py
+++ users/models.py
@@ -6,7 +6,22 @@ class User(models.Model):
     telegram_id = models.BigIntegerField(unique=True)
     api_key = models.CharField(max_length=255, unique=True, null=True, blank=True)
     api_secret = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    pair = models.CharField(
+        "Торговая пара",
+        max_length=30,
+        choices=[
+            ("KAS_USDT", "KAS/USDT"),
+            ("BTC_USDC", "BTC/USDC"),
+        ],
+        blank=True,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
+    profit = models.FloatField(null=True, blank=True)
+    pause = models.IntegerField(null=True, blank=True)
+    loss = models.FloatField(null=True, blank=True)
+    is_active = models.BooleanField(default=False) 
+
 
     def __str__(self):
         return str(self.telegram_id)
