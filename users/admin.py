79d3e5e4258bc5f566ddc6d05d12a0ab82c99f86
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-12 18:34:23 +0300

    api keys for users

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('telegram_id', 'name', 'created_at')
+    list_display = ('telegram_id', 'name', 'created_at', 'api_key', 'api_secret')
--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
     telegram_id = models.BigIntegerField(unique=True)
+    api_key = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    api_secret = models.CharField(max_length=255, unique=True, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
