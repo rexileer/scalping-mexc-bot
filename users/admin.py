a1dd208bda547bcaf5f5d27f9de6cdff91939ba1
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-13 12:13:37 +0300

    fix pair admin

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('telegram_id', 'name', 'created_at', 'api_key', 'api_secret')
+    list_display = ('telegram_id', 'name', 'created_at', 'api_key', 'api_secret', 'pair', 'is_active')
--- users/models.py
+++ users/models.py
@@ -6,16 +6,7 @@ class User(models.Model):
     telegram_id = models.BigIntegerField(unique=True)
     api_key = models.CharField(max_length=255, unique=True, null=True, blank=True)
     api_secret = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    pair = models.CharField(
-        "Торговая пара",
-        max_length=30,
-        choices=[
-            ("KAS_USDT", "KAS/USDT"),
-            ("BTC_USDC", "BTC/USDC"),
-        ],
-        blank=True,
-        null=True,
-    )
+    pair = models.CharField(max_length=30, null=True, blank=True, verbose_name='Торговая пара')
     created_at = models.DateTimeField(auto_now_add=True)
     profit = models.FloatField(null=True, blank=True)
     pause = models.IntegerField(null=True, blank=True)
