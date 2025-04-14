c1ce721c64e8fef46b873c5daec0262d1e5d3e01
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-14 10:43:11 +0300

    Update inline.py

--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -6,5 +6,4 @@ def get_period_keyboard():
         [InlineKeyboardButton(text="📆 Последние 7 дней", callback_data="stats:7d")],
         [InlineKeyboardButton(text="📊 Этот месяц", callback_data="stats:month")],
         [InlineKeyboardButton(text="🕰 Всё время", callback_data="stats:all")],
-        [InlineKeyboardButton(text="📌 Выбрать дату", callback_data="stats:custom")],
     ])
