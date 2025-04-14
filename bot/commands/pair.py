85e748c7cc379ffbfbb79195395171dd8f253686
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-14 16:54:55 +0300

    Update pair.py
    edit instead of answer

--- bot/commands/pair.py
+++ bot/commands/pair.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command
 from users.models import User
+from bot.keyboards.inline import get_pair_keyboard
 
 router = Router()
 
@@ -9,10 +10,7 @@ router = Router()
 @router.message(Command("pair"))
 async def pair(message: Message):
     # Создаем инлайн кнопки для двух торговых пар
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="KAS/USDT", callback_data="pair_KASUSDT")],
-        [InlineKeyboardButton(text="BTC/USDC", callback_data="pair_BTCUSDC")],
-    ])
+    keyboard = get_pair_keyboard()
 
     # Отправка сообщения с клавиатурой
     await message.answer("Выберите торговую пару:", reply_markup=keyboard)
@@ -22,6 +20,7 @@ async def pair(message: Message):
 async def process_pair_selection(callback_query: CallbackQuery):
     # Получаем торговую пару из callback_data
     selected_pair = callback_query.data.split('_')[1]  # Получаем KAS_USDT или BTC_USDC
+    keyboard = get_pair_keyboard()
 
     # Обновляем выбранную пару в базе данных для пользователя
     try:
@@ -29,6 +28,6 @@ async def process_pair_selection(callback_query: CallbackQuery):
         user.pair = selected_pair
         user.save()
         await callback_query.answer(f"Вы выбрали торговую пару: {selected_pair}")
-        await callback_query.message.answer(f"Теперь ваша торговая пара: {selected_pair}")
+        await callback_query.message.edit_text(f"Теперь ваша торговая пара: {selected_pair}", reply_markup=keyboard)
     except Exception as e:
         await callback_query.answer(f"Произошла ошибка: {e}")
