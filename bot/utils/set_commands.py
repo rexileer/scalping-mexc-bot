007fa709b42899571636a7e60aeaa4dc2b82670a
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-23 15:58:21 +0300

    Update set_commands.py
    buttons sorted

--- bot/utils/set_commands.py
+++ bot/utils/set_commands.py
@@ -3,16 +3,16 @@ from aiogram import Bot
 
 async def set_default_commands(bot: Bot):
     commands = [
-        BotCommand(command="/balance", description="Баланс"),
-        BotCommand(command="/price", description="Прайс"),
         BotCommand(command="/autobuy", description="Автопокупка"),
-        BotCommand(command="/stop", description="Остановить автопокупку"),
         BotCommand(command="/buy", description="Покупка"),
         BotCommand(command="/status", description="Статус"),
-        BotCommand(command="/parameters", description="Параметры"),
-        BotCommand(command="/faq", description="FAQ"),
-        BotCommand(command="/subscription", description="Подписка"),
         BotCommand(command="/stats", description="Статистика"),
+        BotCommand(command="/parameters", description="Параметры"),
+        BotCommand(command="/balance", description="Баланс"),
+        BotCommand(command="/price", description="Цена"),
+        BotCommand(command="/subscription", description="Подписка"),
+        BotCommand(command="/stop", description="Стоп"),
+        BotCommand(command="/faq", description="FAQ"),
         BotCommand(command="/set_keys", description="Авторизация MEXC/Установка ключей"),
     ]
     await bot.set_my_commands(commands)
